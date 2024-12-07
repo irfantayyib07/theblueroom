a500b200fb2dfde65957f6c121c1ddd02b3ef3c4
Irfan <irfantayyib07@gmail.com>
2024-12-08 01:27:28 +0500

    simple responsive class (px-6 on header_

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 function RootLayout() {
  return (
   <main className="w-full">
-   <header className="w-full h-24 bg-blue-800 text-white flex justify-center items-center">
+   <header className="px-6 w-full h-24 bg-blue-800 text-white flex justify-center items-center">
     The Blue Room Booking Form with Timetics and Woocommerce
    </header>
    <section className="p-6">
