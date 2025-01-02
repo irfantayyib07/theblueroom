df5683ea1e811431e484d9ece930eb721e46ba68
Irfan <irfantayyib07@gmail.com>
2025-01-02 13:37:14 +0500

    refactor: update HTML structure and enhance layout with new font and footer

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,13 +2,16 @@ import { Outlet } from "react-router-dom";
 
 function RootLayout() {
  return (
-  <main className="w-full">
-   <header className="px-6 w-full h-24 bg-blue-800 text-white flex justify-center items-center">
+  <main className="w-full flex-stretch flex flex-col">
+   {/* <header className="px-6 w-full h-24 bg-blue-800 text-white flex justify-center items-center">
     The Blue Room Booking Form with Timetics and Woocommerce
-   </header>
+   </header> */}
    <section className="p-6">
     <Outlet />
    </section>
+   <footer className="pb-6 flex gap-2 justify-center items-center text-[15px] mt-auto">
+    Powered by <img src="@/../public/images/otbb-logo.png" className="w-44" />
+   </footer>
   </main>
  );
 }
